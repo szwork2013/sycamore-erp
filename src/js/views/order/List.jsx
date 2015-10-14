4693bb817b386422e9253585753001e2fbbeb950
Peter Johnson <peter.johnson@sycamoreconsulting.co.uk>
2015-10-14 16:46:57 +0100

    Add React templates

--- src/js/views/order/List.jsx
+++ src/js/views/order/List.jsx
@@ -9,7 +9,7 @@ var View = React.createClass({
 		var state = {
 			applicationName: "",
 			menus: [],
-			title: "Sycamore ERP - Customers"
+			title: "Sycamore ERP - Orders"
 		};
 
 		if(typeof(locals) != "undefined") {
@@ -29,15 +29,15 @@ var View = React.createClass({
 				<Header applicationName={this.state.applicationName} applicationUrl={this.state.applicationUrl} menus={this.state.menus} />
 				<div className="row">
 					<ul className="breadcrumbs">
-						<li>Customers</li>
+						<li>Orders</li>
 					</ul>
 				</div>
 				<div className="row">
 					<div className="large-10 columns">
-						<h1>Customers</h1>
+						<h1>Orders</h1>
 					</div>
 					<div className="large-2 columns">
-						<a href="/customer/create" className="right fancy radius button tiny">
+						<a href="/order/create" className="right fancy radius button tiny">
 							<i className="in-button-icon fa fa-fw fa-plus"></i> Create
 						</a>
 					</div>
@@ -48,36 +48,10 @@ var View = React.createClass({
 							<div className="table-head">
 								<div className="table-row">
 									<div className="table-cell">Id</div>
-									<div className="table-cell">Display Name</div>
-									<div className="table-cell">Code</div>
-									<div className="table-cell">Name</div>
 									<div className="table-cell large-1">Actions</div>
 								</div>
 							</div>
 							<div className="table-body">
-							{
-								this.props.locals.customers.map(function(customer) {
-									return (
-										<div className="table-row">
-											<div className="table-cell">{customer.Id}</div>
-											<div className="table-cell">{customer.DisplayName}</div>
-											<div className="table-cell">{customer.Code}</div>
-											<div className="table-cell">{customer.Name}</div>
-											<div className="table-cell">
-												<a href={"/customer/view/" + customer._id}>
-													<i className="fa fa-fw fa-user icon-button"></i>
-												</a>
-												<a href={"/customer/edit/" + customer._id}>
-													<i className="fa fa-fw fa-pencil icon-button"></i>
-												</a>
-												<a href={"/customer/delete/" + customer._id}>
-													<i className="fa fa-fw fa-trash-o icon-button"></i>
-												</a>
-											</div>
-										</div>
-									)
-								})
-							}
 							</div>
 						</div>
 					</div>
