81479ce313710426fa0966084926ae000bf2d647
Peter Johnson <peter.johnson@sycamoreconsulting.co.uk>
2015-10-21 13:02:40 +0100

    Update Templates

--- src/js/services/Api.js
+++ src/js/services/Api.js
@@ -3,22 +3,22 @@ var request = require("superagent");
 var Api = {
 	getCustomers: function(queryOptions, callback) {
 		request
-		.get("http://fusion-furniture-solutions.herokuapp.com/sycamore-erp/customers.json")
+		.get("/sycamore-erp/customers.json")
 		.end(callback);
 	},
 	getProducts: function(queryOptions, callback) {
 		request
-		.get("http://fusion-furniture-solutions.herokuapp.com/sycamore-erp/products.json")
+		.get("/sycamore-erp/products.json")
 		.end(callback);
 	},
 	getProperties: function(queryOptions, callback) {
 		request
-		.get("http://fusion-furniture-solutions.herokuapp.com/sycamore-erp/properties.json")
+		.get("/sycamore-erp/properties.json")
 		.end(callback);
 	},
 	getSuppliers: function(queryOptions, callback) {
 		request
-		.get("http://fusion-furniture-solutions.herokuapp.com/sycamore-erp/suppliers.json")
+		.get("/sycamore-erp/suppliers.json")
 		.end(callback);
 	}
 };
