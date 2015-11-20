2a7b45e9f708b69d09d6c50d2337e76effeb5266
Peter Johnson <peter.johnson@sycamoreconsulting.co.uk>
2015-11-20 10:20:25 +0000

    Add loadData

--- src/js/services/Api.js
+++ src/js/services/Api.js
@@ -21,9 +21,81 @@ var Api = {
 		.get("/sycamore-erp/suppliers.json")
 		.end(callback);
 	},
+	putCustomer: function(queryOptions, callback) {
+		request
+		.put("/sycamore-erp/customer")
+		.send(queryOptions)
+		.set('Accept', 'application/json')
+		.set('Content-Type', 'application/json')
+		.end(callback);
+	},
 	putOrder: function(queryOptions, callback) {
 		request
 		.put("/sycamore-erp/order")
+		.send(queryOptions)
+		.set('Accept', 'application/json')
+		.set('Content-Type', 'application/json')
+		.end(callback);
+	},
+	putProduct: function(queryOptions, callback) {
+		request
+		.put("/sycamore-erp/product")
+		.send(queryOptions)
+		.set('Accept', 'application/json')
+		.set('Content-Type', 'application/json')
+		.end(callback);
+	},
+	putProperty: function(queryOptions, callback) {
+		request
+		.put("/sycamore-erp/property")
+		.send(queryOptions)
+		.set('Accept', 'application/json')
+		.set('Content-Type', 'application/json')
+		.end(callback);
+	},
+	putSupplier: function(queryOptions, callback) {
+		request
+		.put("/sycamore-erp/supplier")
+		.send(queryOptions)
+		.set('Accept', 'application/json')
+		.set('Content-Type', 'application/json')
+		.end(callback);
+	},
+	postCustomer: function(queryOptions, callback) {
+		request
+		.post("/sycamore-erp/customer")
+		.send(queryOptions)
+		.set('Accept', 'application/json')
+		.set('Content-Type', 'application/json')
+		.end(callback);
+	},
+	postOrder: function(queryOptions, callback) {
+		request
+		.post("/sycamore-erp/order")
+		.send(queryOptions)
+		.set('Accept', 'application/json')
+		.set('Content-Type', 'application/json')
+		.end(callback);
+	},
+	postProduct: function(queryOptions, callback) {
+		request
+		.post("/sycamore-erp/product")
+		.send(queryOptions)
+		.set('Accept', 'application/json')
+		.set('Content-Type', 'application/json')
+		.end(callback);
+	},
+	postProperty: function(queryOptions, callback) {
+		request
+		.post("/sycamore-erp/property")
+		.send(queryOptions)
+		.set('Accept', 'application/json')
+		.set('Content-Type', 'application/json')
+		.end(callback);
+	},
+	postSupplier: function(queryOptions, callback) {
+		request
+		.post("/sycamore-erp/supplier")
 		.send(queryOptions)
 		.set('Accept', 'application/json')
 		.set('Content-Type', 'application/json')
