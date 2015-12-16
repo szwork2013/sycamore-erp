fab4df6763a53f38f46059cd1f6a662d4959d17d
Peter Johnson <peter.johnson@sycamoreconsulting.co.uk>
2015-12-16 11:42:28 +0000

    Send email order

--- app/controllers/orderController.js
+++ app/controllers/orderController.js
@@ -129,6 +129,7 @@ orderController.prototype.sendEmailOrderAction = function(request, response, nex
 			// Mandrill returns the error as an object with name and message keys
 			console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
 			// A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+			next(e);
 		}
 	);
 }
